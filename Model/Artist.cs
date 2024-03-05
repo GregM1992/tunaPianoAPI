6f465ab79926fd574b54781b98a9f2556b4aa7cc
Gregory Markus <gregmarkus1992@yahoo.com>
2024-03-04 18:52:42 -0600

    Add project files.

---
+++ Model/Artist.cs
@@ -0,0 +1,11 @@
+﻿namespace tunaPianoAPI.Model
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public string Bio { get; set;}
+
+    }
+}
---
+++ Model/Song.cs
@@ -0,0 +1,13 @@
+namespace tunaPianoAPI.Model
+{
+    public class Song
+    {
+        public string Title { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public string Album { get; set; }
+
+        public int Length { get; set; }
+    }
+}
